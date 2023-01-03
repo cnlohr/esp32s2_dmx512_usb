a3bc88e838411fe15449cd4e27f42292ab33b58f
cnlohr <lohr85@gmail.com>
2023-01-03 01:51:30 -0500

    I HATE UARTS IS BROKEN

--- main/main.c
+++ main/main.c
@@ -15,11 +15,21 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-
 #include "tinyusb.h"
 #include "tusb_hid_gamepad.h"
+#include "driver/uart.h"
+#include "hal/gpio_types.h"
+#include "driver/gpio.h"
+#include "rom/gpio.h"
 
-uint8_t dmx_buffer[1025];
+#define DMX_UART UART_NUM_1
+
+#define MAX_DMX 1024
+uint8_t dmx_buffer[1024 + 4] = { 0 };
+volatile int highest_send = 0;
+
+SemaphoreHandle_t DoneSendMutex;
+SemaphoreHandle_t PleaseSendMutex;
 
 uint16_t tud_hid_get_report_cb(uint8_t itf,
 							   uint8_t report_id,
@@ -30,6 +40,7 @@ uint16_t tud_hid_get_report_cb(uint8_t itf,
 	if( report_id == 0xad )
 	{
 		// DMX packet
+		// No special packet supported.
 	}
 	else
 	{
@@ -45,18 +56,30 @@ void tud_hid_set_report_cb(uint8_t itf,
 						   uint8_t const* buffer,
 						   uint16_t bufsize )
 {
-	if( report_id == 0xad )
+	// Code 0xad has a special setting in TUD_HID_REPORT_DESC_GAMEPAD to allow unrestricted HIDAPI access from Windows.
+	if( report_id == 0xad && buffer[1] == 0x73 )
 	{
 		// DMX packet
-		int offset = buffer[0] * 4;
-		if( offset + bufsize > sizeof( dmx_buffer ) )
+		int offset = buffer[2] * 4;
+		int towrite = buffer[3];
+		if( offset + towrite > sizeof( dmx_buffer ) )
+			towrite = sizeof( dmx_buffer ) - offset;
+		memcpy( dmx_buffer + offset + 4, buffer + 4, towrite );
+
+		if( offset + towrite > highest_send ) highest_send = offset + towrite;
+
+		//printf( "%02x %02x %02x %02x %d %d %d %d %d\n", buffer[0], buffer[1], buffer[2], buffer[3], highest_send, offset, bufsize, buffer[bufsize], towrite );
+
+		if( offset == 0 )
 		{
-			
+			// Actually send.
+			xSemaphoreTake( DoneSendMutex, portMAX_DELAY );
+			xSemaphoreGive( PleaseSendMutex );
 		}
 	}
 	else
 	{
-		printf( "Custom Report Set %d %d %02x [%d]\n", itf, report_id, report_type,bufsize );
+		printf( "Custom Report Set %d %d[%02x] %02x [%d]\n", itf, report_id, buffer[0], report_type, bufsize );
 	}
 }
 
@@ -65,16 +88,50 @@ void app_main(void)
 	printf( "Starting\n" );
 	fflush(stdout);
 
+	PleaseSendMutex = xSemaphoreCreateBinary();
+	DoneSendMutex = xSemaphoreCreateBinary();
+
+	uart_config_t uart_config = {
+		.baud_rate = 250000,
+		.data_bits = UART_DATA_8_BITS,
+		.parity = UART_PARITY_DISABLE,
+		.stop_bits = UART_STOP_BITS_1,
+		.flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+	};
+
+	ESP_ERROR_CHECK(uart_param_config(DMX_UART, &uart_config));
+	ESP_ERROR_CHECK(uart_set_pin(DMX_UART, 18, -1, -1, -1));
+
+	QueueHandle_t uart_queue;
+	// Install UART driver using an event queue here
+	ESP_ERROR_CHECK(uart_driver_install(DMX_UART, 256, 256, 10, &uart_queue, 0));
+
+	// Use the IO matrix to create the inverse of TX on pin 17.
+	gpio_matrix_out( GPIO_NUM_17, U1TXD_OUT_IDX, 1, 0 );
+
+	// Maximize the drive strength.
+	gpio_set_drive_capability( GPIO_NUM_17, GPIO_DRIVE_CAP_3 );
+	gpio_set_drive_capability( GPIO_NUM_18, GPIO_DRIVE_CAP_3 );
+
+
 	/* Initialize USB peripheral */
 	tinyusb_config_t tusb_cfg = {};
 	tinyusb_driver_install(&tusb_cfg);
 
+//	vTaskSuspend( 0 );
+
+//	uart_write_bytes_with_break(DMX_UART, "", 1, 20 ); // Can't use uart_write_bytes_with_break because break needs to go right before frame.
+
 	/* Main loop */
-	while(true) {
-		printf( "Tick\n" );
-		// Yield to let the rest of the RTOS run
-		//taskYIELD();
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	while(true)
+	{
+		xSemaphoreGive( DoneSendMutex );
+		xSemaphoreTake( PleaseSendMutex, portMAX_DELAY );
+		uart_wait_tx_done(DMX_UART, portMAX_DELAY);
+		ets_delay_us( 140 );
+		// I have literally no idea why this needs to be called from the main task instead of the USB task.
+		uart_write_bytes_with_break(DMX_UART, dmx_buffer+3, highest_send+1, 20 );
+		highest_send = 0;
 	}
 	return;
 }
---
+++ testapp/dmx512testapp.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "hidapi.h"
+#include "hidapi.c"
+
+#define VID 0x303a
+#define PID 0x4004
+
+hid_device * hd;
+
+int tries = 0;
+int alignlen = 4;
+
+int req_sandbox = 1024;
+
+
+uint32_t EHSVtoHEX( uint8_t hue, uint8_t sat, uint8_t val );
+
+int main( int argc, char ** argv )
+{
+	int r;
+	hid_init();
+	hd = hid_open( VID, PID, L"cndmx512v001");
+	if( !hd ) { fprintf( stderr, "Could not open USB\n" ); return -94; }
+	// Disable mode.
+	uint8_t colodata[800] = { 0 };
+	int channels = 80;
+	int frame = 0;
+	do
+	{
+		frame++;
+		int i;
+		for( i = 0; i < 512/3+1; i++ )
+		{
+			int color = EHSVtoHEX( i + frame, 0xff, 0xff );
+			colodata[i*3+0] = 0x10;//color & 0xff;
+			colodata[i*3+1] = 0x10;//(color>>8) & 0xff;
+			colodata[i*3+2] = 0x10;//(color>>16) & 0xff;
+		}
+	
+
+		int chunk;
+		int chunks = (channels + 249) / 250;
+		for( chunk = chunks-1; chunk >= 0; chunk-- )
+		{
+			// Upon sending 0th chunk, it will transmit the acutal signal.
+			uint8_t rdata[256] = { 0 };
+
+			int offset = chunk * 250;
+			int remain = channels - offset;
+			if( remain > 250 ) remain = 250;
+
+			rdata[0] = 0xad;  // Feature Report ID
+			rdata[1] = 0x73;
+			rdata[2] = offset/4;
+			rdata[3] = remain;
+			memcpy( rdata+4, colodata + chunk * 250, 250 );
+			//printf( "%d %d rdata[10] = %02x\n", offset, remain, rdata[15] );
+			do
+			{
+				r = hid_send_feature_report( hd, rdata, 255 );
+				if( tries++ > 10 ) { fprintf( stderr, "Error sending feature report on command %d (%d)\n", rdata[1], r ); return -85; }
+			} while ( r < 6 );
+			tries = 0;
+		}
+		printf( "." ); fflush( stdout );
+	} while( 1 );
+	
+}
+
+
+
+
+uint32_t EHSVtoHEX( uint8_t hue, uint8_t sat, uint8_t val )
+{
+	#define SIXTH1 43
+	#define SIXTH2 85
+	#define SIXTH3 128
+	#define SIXTH4 171
+	#define SIXTH5 213
+
+	uint16_t or = 0, og = 0, ob = 0;
+
+	hue -= SIXTH1; //Off by 60 degrees.
+
+	//TODO: There are colors that overlap here, consider 
+	//tweaking this to make the best use of the colorspace.
+
+	if( hue < SIXTH1 ) //Ok: Yellow->Red.
+	{
+		or = 255;
+		og = 255 - ((uint16_t)hue * 255) / (SIXTH1);
+	}
+	else if( hue < SIXTH2 ) //Ok: Red->Purple
+	{
+		or = 255;
+		ob = (uint16_t)hue*255 / SIXTH1 - 255;
+	}
+	else if( hue < SIXTH3 )  //Ok: Purple->Blue
+	{
+		ob = 255;
+		or = ((SIXTH3-hue) * 255) / (SIXTH1);
+	}
+	else if( hue < SIXTH4 ) //Ok: Blue->Cyan
+	{
+		ob = 255;
+		og = (hue - SIXTH3)*255 / SIXTH1;
+	}
+	else if( hue < SIXTH5 ) //Ok: Cyan->Green.
+	{
+		og = 255;
+		ob = ((SIXTH5-hue)*255) / SIXTH1;
+	}
+	else //Green->Yellow
+	{
+		og = 255;
+		or = (hue - SIXTH5) * 255 / SIXTH1;
+	}
+
+	uint16_t rv = val;
+	if( rv > 128 ) rv++;
+	uint16_t rs = sat;
+	if( rs > 128 ) rs++;
+
+	//or, og, ob range from 0...255 now.
+	//Need to apply saturation and value.
+
+	or = (or * val)>>8;
+	og = (og * val)>>8;
+	ob = (ob * val)>>8;
+
+	//OR..OB == 0..65025
+	or = or * rs + 255 * (256-rs);
+	og = og * rs + 255 * (256-rs);
+	ob = ob * rs + 255 * (256-rs);
+//printf( "__%d %d %d =-> %d\n", or, og, ob, rs );
+
+	or >>= 8;
+	og >>= 8;
+	ob >>= 8;
+
+	return or | (og<<8) | ((uint32_t)ob<<16);
+}
+
